fae3276f1fc9c2773fff55ae2524e6aac84e044c
Kaushik Bhat <128929833+kaushik-bhat@users.noreply.github.com>
2024-02-17 12:31:27 +0530

    Basic working with Save button

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import tkinter as tk
+from tkinter import filedialog
 
 class CollaborativeTextEditor(tk.Tk):
     def __init__(self):
@@ -19,6 +20,13 @@ class CollaborativeTextEditor(tk.Tk):
 
         self.text_widget.bind("<Key>", self.on_key_press)
 
+        # Add a menu bar
+        self.menu_bar = tk.Menu(self)
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.file_menu.add_command(label="Save", command=self.save_file)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.config(menu=self.menu_bar)
+
     def receive_messages(self):
         while True:
             try:
@@ -36,6 +44,14 @@ class CollaborativeTextEditor(tk.Tk):
     def send_text(self):
         text_content = self.text_widget.get("1.0", "end-1c")  # Get the entire text content
         self.client.send(text_content.encode('utf-8'))
+
+    def save_file(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+        if file_path:
+            text_content = self.text_widget.get("1.0", "end-1c")
+            with open(file_path, "w") as file:
+                file.write(text_content)
+            print("File saved successfully.")
 
     def destroy(self):
         self.client.send('exit'.encode('utf-8'))
